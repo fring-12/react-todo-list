5853471b8efb64a75619b920c7cac2c8ebb2a420
Banedict Fring Drong <banedictfring12@gmail.com>
2019-11-18 22:25:25 +0600

    all method implemented

--- src/App.js
+++ src/App.js
@@ -1,49 +1,60 @@
-import React, {Component} from 'react';
-import './App.css';
-import  'bootstrap/dist/css/bootstrap.min.css';
-import uuid from 'uuid';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
-
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 class App extends Component {
   state = {
     items: [],
     id: uuid(),
-    item:'',
-    editItem:false
+    item: "",
+    editItem: false
   };
-  handleChange = (e) =>{
+  handleChange = e => {
     this.setState({
       item: e.target.value
     });
-  }
-  handleSubmit = (e)=> {
-    e.preventDefault(); 
+  };
+  handleSubmit = e => {
+    e.preventDefault();
 
     const newItem = {
       id: this.state.id,
       item: this.state.item
-    }
-     
-    const updateItem = [...this.state.items,newItem]
+    };
+
+    const updateItem = [...this.state.items, newItem];
 
     this.setState({
       items: updateItem,
       id: uuid(),
-      item:'',
-      editItem:false
-    }, ()=> console.log(this.state));
-  }
-  clearList = ()=> {
-    console.log('clear list')
-  }
-  handleDelete = (id)=> {
-    console.log(`handle delete ${id}`)
-  }
-  handleEdit = (id)=> {
-    console.log(`handle delete ${id}`)
-  }
+      item: "",
+      editItem: false
+    });
+  };
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  };
+  handleDelete = id => {
+    const filterItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filterItems
+    });
+  };
+  handleEdit = id => {
+    const filterItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filterItems,
+      item: selectedItem.item,
+      id: id,
+      editItem: true
+    });
+  };
   render() {
     console.log(this.state);
 
@@ -51,28 +62,25 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto com-md-8 mt-5 ">
-              <h3 className="text-capitalize text-center">todo list</h3>
-              <TodoInput
-               item={this.state.item}
-               handleChange={this.handleChange} 
-               handleSubmit={this.handleSubmit} 
-               editItem={this.state.editItem}
-               />
+            <h3 className="text-capitalize text-center">todo list</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
 
-              <TodoList
-               items={this.state.items}
-               clearList={this.clearList}
-               handleDelete={this.handleDelete}
-               handleEdit={this.handleEdit}
-               />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
-         
         </div>
-        
       </div>
     );
   }
 }
 
 export default App;
-
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class extends Component {
-    render() {
-        const {item, handleChange, handleSubmit, editItem} = this.props;
-        return (
-            <div className="card card-body my-3">
-                <form onSubmit={handleSubmit}>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text bg-primary text-white">
-                                <i className="fas fa-book" />
-                            </div>
-                        </div>
-                        <input className="form-control text-capitalize"
-                         placeholder="Add item to todo list" value={item}
-                         onChange={handleChange}  />
-                    </div>
-                    <button className="btn btn-block btn-primary mt-3 text-uppercase"
-                    type="submit">add item</button>
-                </form>
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
             </div>
-        )
-    }
+            <input
+              className="form-control text-capitalize"
+              placeholder="Add item to todo list"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className={
+              editItem
+                ? "btn btn-block btn-success mt-3 text-uppercase"
+                : "btn btn-block btn-primary mt-3 text-uppercase"
+            }
+            type="submit"
+            disabled={item ? false : true}
+          >
+            {editItem ? "edit item" : "add item"}
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
